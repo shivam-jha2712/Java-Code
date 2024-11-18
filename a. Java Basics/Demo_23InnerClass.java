ba79ce130652a1d7c37dde7324ebbac196d5c2d6
shivam-jha2712 <shivam.jha.2712@gmail.com>
2024-11-19 02:47:44 +0530

    commit-Inner-class.java

---
+++ a. Java Basics/Demo_23InnerClass.java
@@ -0,0 +1,19 @@
+class A {
+    int age;
+
+    public void show() {
+        System.out.println("in A show");
+    }
+
+    class B {
+        public void show2() {
+            System.out.println("in B config");
+        }
+    }
+}
+
+public class Demo_23InnerClass {
+    public static void main(String[] args) {
+
+    }
+}
